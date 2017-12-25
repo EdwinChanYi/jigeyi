edb7bd1f47f229e1745278beda88d92664170c98
edwinchan <568264623@qq.com>
2017-12-25 17:15:18 +0800

    zhihong

--- model/BaseModel.py
+++ model/BaseModel.py
@@ -9,111 +9,94 @@ import pymysql
 
 class BaseModel(object):
 
-    __instance = None
-
-    # 存放连接字典
-    conn = {}
-
     # 默认数据库,次于mod参数
-    __default_mod = None
+    __db = None
 
-    def __new__(cls, *args, **kwargs):
-        if cls.__instance is None:
-            cls.__instance = object.__new__(cls, *args, **kwargs)
-        return cls.__instance
+    # __ins = None
 
-    # 统一使用此方式实例话
+    # 获取单例
     # @classmethod
     # def instance(cls):
-    #     if not hasattr(cls, '_instance'):
-    #         cls._instance = cls()
-    #     return cls._instance
+    #     if cls.__ins is None:
+    #         cls.__ins = cls()
+    #     return cls.__ins
+
+    def __init__(self, db=None):
+        self.__db = db
+
+    def get_db(self):
+        return self.__db
 
     # 设置默认使用的库
     def setDefaultMod(self, mod):
         if mod:
-            self.__default_mod = mod
+            self.__db = mod
 
     # 获取连接
-    async def getConn(self, mod):
-        if not mod and self.__default_mod:
-            mod = self.__default_mod
-        if self.conn.get(mod) is None:
-            self.conn[mod] = await Db.conn(mod)
-        return self.conn[mod]
+    def getConn(self, mod=None):
+        if mod is None and self.__db:
+            mod = self.__db
+        return Db.instance().conn(mod)
 
-    # 获取游标
-    async def getCursor(self, mod, cursor_type=pymysql.cursors.DictCursor):
-        if not mod and self.__default_mod:
-            mod = self.__default_mod
-        if not mod:
-            raise Exception('no mod to get cursor');
-        conn = await self.getConn(mod)
-        return conn.cursor(cursor_type)
-
-    async def one(self, sql, param, mod):
-        if not mod and self.__default_mod:
-            mod = self.__default_mod
-        cursor = await self.getCursor(mod)
+    async def one(self, sql, param, mod=None):
+        if mod is None and self.__db:
+            mod = self.__db
+        conn = self.getConn(mod)
+        cursor = conn.cursor(pymysql.cursors.DictCursor)
         cursor.execute(sql, param)
         res = cursor.fetchone()
         cursor.close()
+        conn.close()
         return res
 
-    async def all(self, sql, param, mod):
-        if not mod and self.__default_mod:
-            mod = self.__default_mod
-        cursor = await self.getCursor(mod)
+    async def all(self, sql, param, mod=None):
+        if mod is None and self.__db:
+            mod = self.__db
+        conn = self.getConn(mod)
+        cursor = conn.cursor(pymysql.cursors.DictCursor)
         cursor.execute(sql, param)
         res = cursor.fetchall()
         cursor.close()
+        conn.close()
         return res
 
-    async def insert(self, sql, param, mod):
-        if not mod and self.__default_mod:
-            mod = self.__default_mod
-        cursor = await self.getCursor(mod)
+    async def insert(self, sql, param, mod=None):
+        if mod is None and self.__db:
+            mod = self.__db
+        conn = self.getConn(mod)
+        cursor = conn.cursor(pymysql.cursors.DictCursor)
         row = cursor.execute(sql, param)
         if row != 1:
             return False
         cursor.close()
+        conn.close()
         insert_id = cursor.lastrowid
         return insert_id
 
-    async def update(self, sql, param, mod):
-        if not mod and self.__default_mod:
-            mod = self.__default_mod
-        cursor = await self.getCursor(mod)
+    async def update(self, sql, param, mod=None):
+        if not mod and self.__db:
+            mod = self.__db
+        conn = self.getConn(mod)
+        cursor = conn.cursor(pymysql.cursors.DictCursor)
         row = cursor.execute(sql, param)
         cursor.close()
-        if not row or row <= 0:
-            return False
-        return True
+        conn.close()
+        return row
 
-    async def delete(self, sql, param=()):
-        cursor = await self.get_cursor(self.__master_mod)
+    async def delete(self, sql, param=(), mod=None):
+        if not mod and self.__db:
+            mod = self.__db
+        conn = self.getConn(mod)
+        cursor = conn.cursor(pymysql.cursors.DictCursor)
         row = cursor.execute(sql, param)
         cursor.close()
-        if not row or row <= 0:
-            return False
-        return True
-
-    async def commit(self):
-        conn = await self.get_conn(self.__master_mod)
-        conn.commit()
-
-    async def rollback(self):
-        conn = await self.get_conn(self.__master_mod)
-        conn.rollback()
-
-    @classmethod
-    def close(self):
-        for mod in self.conn:
-            self.conn.get(mod).close()
-
+        conn.close()
+        return row
 
 if (__name__ == '__main__'):
-    a = BaseModel.instance()
-    res = a.insert('insert into test(id,name) values(%s, %s)', (10, 'zhou'))
-    res = a.one('select * from test')
-    print(res)
+    a = BaseModel()
+    b = BaseModel()
+    print(a == b)
+    # res = a.insert('insert into test(id,name) values(%s, %s)', (10, 'zhou'))
+    # res = a.one('select * from test')
+    # print(res)
