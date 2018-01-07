dca8e962e06cd1ed8659dd1a025070d000ffc4c0
edwinchan <568264623@qq.com>
2018-01-07 20:33:37 +0800

    sql

--- sql/2_recipe_create.sql
+++ sql/2_recipe_create.sql
@@ -1,12 +1,12 @@
-CREATE TABLE `recipe_` (
+CREATE TABLE `recipe_%s` (
   `material_id` int(11) unsigned NOT NULL DEFAULT '0' COMMENT '食材id',
   `recipe_id` int(11) unsigned NOT NULL DEFAULT '0' COMMENT '菜谱id',
-  `recipe_name` VARCHAR(255) NOT NULL DEFAULT '' COMMIT '菜谱名'
-  `material_name` VARCHAR(255) NOT NULL DEFAULT '' COMMIT '食材名'
-  `image` VARCHAR(255) DEFAULT '' COMMIT '图片 ps:json格式存储'
-  `description` VARCHAR(255) DEFAULT '' COMMIT '描述 ps:json格式存储'
+  `recipe_name` VARCHAR(255) NOT NULL DEFAULT '' COMMENT '菜谱名',
+  `material_name` VARCHAR(255) NOT NULL DEFAULT '' COMMENT '食材名',
+  `image` VARCHAR(255) DEFAULT '' COMMENT '图片 ps:json格式存储',
+  `description` VARCHAR(255) DEFAULT '' COMMENT '描述 ps:json格式存储',
   `create_time` int(11) NOT NULL DEFAULT '0' COMMENT '创建时间',
   `update_time` int(11) DEFAULT NULL COMMENT '最近更新时间',
-  PRIMARY KEY (`material_id`,`kind_id`),
+  PRIMARY KEY (`material_id`,`recipe_id`),
   KEY (`material_name`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='用户表'
