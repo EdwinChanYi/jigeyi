2e699dd2b4cfe049b2396fa4817c1e58fd097640
liuzhihongSky <eliuzhihong@163.com>
2018-01-07 21:46:24 +0800

    fix

---
+++ module/TestModule.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+#-*- coding: utf-8 -*-
+# 测试模块
+
+from module import BaseModule
+from model import UserModel
+from common.Decorate import redisGet,redisHget,redisHashObj
+
+class TestModule(BaseModule):
+
+    @redisHget('a_%s', 'b', ['1'], True, True, 3600)
+    async def getUserInfo(self, id):
+        user_model = UserModel(self._db)
+        user = await user_model.findById(id)
+        return user
